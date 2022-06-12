aa171c02400ee5a95311839a021b70a2fcf6b9c7
honesty-yoo <yhm9207@naver.com>
2022-06-12 17:54:00 +0900

    Compare The Triplets 문제 : 리팩토링
    - Util 클래스 추가
    - validation 메소드 분리
    - 비교 로직 메소드 분리
    - 리팩토링 사유 주석 추가
    - Hackerrank 목표 추가

--- src/yoo/hackerrank/practice/CompareTheTriplets.java
+++ src/yoo/hackerrank/practice/CompareTheTriplets.java
@@ -2,7 +2,6 @@ package yoo.hackerrank.practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Compare The Triplets
@@ -11,56 +10,93 @@ import java.util.Objects;
  * 평가항목은 정확성, 독창성, 어려움입니다.
  * 3가지 항목을 평가하여 검토자들이 비교를 합니다.
  *
- * 조건 : 1 <= score <= 100
+ * 조건 : 1 <= rate <= 100
+ * 리팩토링
+ * 1차 목표 : 클래스와 메소드가 한 개의 책임만 가질 수 있도록 분리
  *
  * 문제 링크 : https://www.hackerrank.com/challenges/compare-the-triplets/problem
  */
 public class CompareTheTriplets {
 
+    private final int FIRST_INDEX = 0;
+    private final int RATE_SIZE = 3;
+
     /**
      * Alice와 Bob의 도전 등급 비교
      *
-     * @param a Alice 점수
-     * @param b Bob 점수
+     * @param a Alice rate
+     * @param b Bob rate
      * @return Alice와 Bob의 좋게 평가 받은 count
      */
     public List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-        int aliceScoresCnt = 0;
-        int bobScoresCnt = 0;
+        int aliceWellRateCnt = 0, bobWellRateCnt = 0;
 
-        for (int index = 0; index < a.size(); index++) {
-            Integer aliceScore = a.get(index);
-            Integer bobScore = b.get(index);
+        for (int index = FIRST_INDEX; index < RATE_SIZE; index++) {
+            Integer aliceRate = a.get(index);
+            Integer bobRate = b.get(index);
 
-            if (isNotRatingConstraint(aliceScore)) {
-                throw new RuntimeException("Alice의 등급을 잘못 입력하였습니다.");
+            validateConstraint(aliceRate, bobRate);
+
+            if (isAliceLargerThanBob(aliceRate, bobRate)) {
+                aliceWellRateCnt++;
             }
 
-            if (isNotRatingConstraint(bobScore)) {
-                throw new RuntimeException("Bob의 등급을 잘못 입력하였습니다.");
-            }
-
-            if (Objects.equals(aliceScore, bobScore)) {
-                continue;
-            }
-
-            if (aliceScore > bobScore) {
-                aliceScoresCnt++;
-            }
-
-            if (aliceScore < bobScore) {
-                bobScoresCnt++;
+            if (isBobLargerThanAlice(aliceRate, bobRate)) {
+                bobWellRateCnt++;
             }
         }
 
-        return Arrays.asList(aliceScoresCnt, bobScoresCnt);
+        return Arrays.asList(aliceWellRateCnt, bobWellRateCnt);
     }
 
-    private boolean isRatingConstraint(Integer rate) {
-        return rate >= 1 && rate <= 100;
+    /**
+     * 밥이 앨리스보다 등급이 큰지 여부
+     *
+     * @param aliceRate 앨리스 등급
+     * @param bobRate 밥 등급
+     * @return 밥이 앨리스보다 등급이 큰지 여부
+     */
+    private boolean isBobLargerThanAlice(Integer aliceRate, Integer bobRate) {
+        return aliceRate < bobRate;
     }
 
-    private boolean isNotRatingConstraint(Integer rate) {
-        return !isRatingConstraint(rate);
+    /**
+     * 앨리스보다 밥이 등급이 큰지 여부
+     *
+     * @param aliceRate 앨리스 등급
+     * @param bobRate 밥 등급
+     * @return 앨리스보다 밥이 등급이 큰지 여부
+     */
+    private boolean isAliceLargerThanBob(Integer aliceRate, Integer bobRate) {
+        return aliceRate > bobRate;
+    }
+
+    /**
+     * 1 <= rate <= 100 제약 사항 검증
+     *
+     * @param aliceRate 앨리스의 등급
+     * @param bobRate 밥의 등급
+     */
+    private void validateConstraint(Integer aliceRate, Integer bobRate) {
+        if (CompareTheTripletsUtils.isNotRatingConstraint(aliceRate)) {
+            throw new RuntimeException("Alice의 등급을 잘못 입력하였습니다.");
+        }
+
+        if (CompareTheTripletsUtils.isNotRatingConstraint(bobRate)) {
+            throw new RuntimeException("Bob의 등급을 잘못 입력하였습니다.");
+        }
+    }
+
+    /**
+     * CompareTheTriplets의 Utils
+     */
+    public static class CompareTheTripletsUtils {
+        public static boolean isRatingConstraint(Integer rate) {
+            return rate >= 1 && rate <= 100;
+        }
+
+        public static boolean isNotRatingConstraint(Integer rate) {
+            return !isRatingConstraint(rate);
+        }
     }
 }
